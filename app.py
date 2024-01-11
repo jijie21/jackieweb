be28b9308fd2bc0a26a13fd9319423cc4fce871d
jijie21 <jennyjijie21@gmail.com>
2024-01-11 14:03:41 -0800

    image

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ st.title('This is a Jackie')
 st.title('_Streamlit_ is :blue[cool] :sunglasses:')
 
 # Load the image
-image = Image.open('/Users/jieji/Library/Mobile Documents/com~apple~CloudDocs/UW Winter 2024/TECHIN510/Lab 1/IMG_0969.JPG')
+image = Image.open('./IMG_0969.JPG')
 
 # Display the image
 st.image(image)
